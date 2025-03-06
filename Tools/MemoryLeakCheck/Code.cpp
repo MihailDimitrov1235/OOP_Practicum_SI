528a406f013d2feadf2dbf8c2330baf56310c8ad
GeorgiLazov <georgilazov04@gmail.com>
2025-03-06 11:07:50 -0800

    Update Code.cpp

--- Tools/MemoryLeakCheck/Code.cpp
+++ Tools/MemoryLeakCheck/Code.cpp
@@ -6,7 +6,7 @@
 //IMPORTANT: only works when debugging
 int main() //so set break point for debugging here. Debug and then step out (arrow up) to exit the program 
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
   //...
   //someLogic("crit1.txt", "crit2.txt", "res.txt"); The function has no memory leak
